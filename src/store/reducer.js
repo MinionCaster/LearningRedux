8615acd2428fabb74a1e9224a1f52950d062a553
Psych00l <35641863+Psych00l@users.noreply.github.com>
2018-08-12 09:15:55 +0200

    redux action

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -3,6 +3,24 @@ const initialState = {
 }
 
 const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case 'INCREMENT': 
+            return {
+                counter: state.counter + 1
+            }
+        case 'DECREMENT':
+            return {
+                counter: state.counter - 1
+            }
+        case 'ADD_TO':
+            return {
+                counter: state.counter + action.val
+            }
+        case 'REMOVE_FROM':
+            return {
+                counter: state.counter - action.val
+            }
+    }
     return state;
 }
 
